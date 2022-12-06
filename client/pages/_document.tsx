ef5354f77b4d5eecb2683b97120c475361ce9d23
yashiroryo <>
2022-12-07 07:22:34 +0900

    feat: google font の導入

---
+++ client/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+// class Document extends NextDocument {
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
