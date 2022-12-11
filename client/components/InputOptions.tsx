c922310d5018de1bcd55426aa5157e0de5f04703
yashiroryo <>
2022-12-11 19:59:45 +0900

    feat: input selectコンポーネントを追加

---
+++ client/components/InputOptions.tsx
@@ -0,0 +1,28 @@
+import { Form } from "react-bootstrap";
+
+type Props = {
+  formLabel: string;
+  options: Array<Option>;
+};
+
+type Option = {
+  value: number | string;
+  label: string;
+};
+
+export default function InputOptions(props: Props) {
+  return (
+    <>
+      <Form.Label>{props.formLabel}</Form.Label>
+      <Form.Select>
+        {props.options.map((option: Option, index: number) => {
+          return (
+            <option value={option.value} key={index}>
+              {option.label}
+            </option>
+          );
+        })}
+      </Form.Select>
+    </>
+  );
+}
--- client/components/TaskEditor.tsx
+++ client/components/TaskEditor.tsx
@@ -1,6 +1,7 @@
 import { Modal, Button } from "react-bootstrap";
 import styled from "styled-components";
 import InputForm from "./InputForm";
+import InputOptions from "./InputOptions";
 
 type Props = {
   isVisible: boolean;
@@ -16,14 +17,33 @@ export default function TaskEditor(props: Props) {
     color: #636363;
   `;
 
+  const options = [
+    {
+      label: "高",
+      value: "high",
+    },
+    {
+      label: "中",
+      value: "middium",
+    },
+    {
+      label: "低",
+      value: "low",
+    },
+  ];
+
   return (
     <StyledModal show={props.isVisible} onHide={handleClose}>
       <StyledModal.Header closeButton>
         <StyledModal.Title>タスク編集</StyledModal.Title>
       </StyledModal.Header>
       <StyledModal.Body>
-        <InputForm formLabel="label" formType="text" placeHolder="text" />
-        <InputForm formLabel="label" formType="text" placeHolder="text" />
+        <InputForm
+          formLabel="タスク名"
+          formType="text"
+          placeHolder="例: ご飯作る"
+        />
+        <InputOptions formLabel="優先度" options={options} />
       </StyledModal.Body>
       <StyledModal.Footer>
         <Button variant="secondary" onClick={handleClose}>
